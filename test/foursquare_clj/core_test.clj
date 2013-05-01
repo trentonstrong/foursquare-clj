302cfc58a7bf21cc403f69bb1011a838e2e3d897
Trenton Strong <strong@okcupidlabs.com>
2013-05-01 10:24:20 -0700

    Leiningen project structure

---
+++ test/foursquare_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns foursquare-clj.core-test
+  (:require [clojure.test :refer :all]
+            [foursquare-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
